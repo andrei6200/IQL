1e3a1fb39319e586f9d10ee7f0768358b1e411f3
Andrei Aiordachioaie <a.aiordachioaie@jacobs-university.de>
2010-06-12 17:22:24 +0200

    Initialization checks consistency of table names in rasdaman and postgres.

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,12 @@
+/* Various C++ Standard Library includes */
 #include <iostream>
 #include <cstdio>
+#include <set>
+#include <string>
+
 
 /* PostgreSQL includes and defines */
 #include <pqxx/pqxx>
-
 
 /* Rasdaman includes and defines */
 #define DEBUG_MAIN
@@ -34,7 +37,7 @@ void runSqlQuery(char* query)
 {
     connection C;
 
-    cout << "Connected to database." << endl
+    cout << "Connected to database: " << C.dbname() << endl
         << "Backend version: " << C.server_version() << endl
         << "Protocol version: " << C.protocol_version() << endl;
 
@@ -130,21 +133,154 @@ void runRasqlQuery(char* queryString)
 }
 
 
-int main()
+set<string> listRasdamanCoverages()
 {
-	yyparse();
+    std::set<std::string> tableSet;
 
-        if (hqlQueries == NULL)
+    const char query[] = "select mddcollname from RAS_MDDCOLLNAMES;";
+    connection C("dbname=RASBASE");
+
+    cout << "Connected to database: " << C.dbname() << endl
+        << "Backend version: " << C.server_version() << endl
+        << "Protocol version: " << C.protocol_version() << endl << endl;
+
+    // Begin a transaction acting on our current connection.  Give it a human-
+    // readable name so the library can include it in error messages.
+    nontransaction T(C, "test1");
+
+    // Perform the query on the database, storing result tuples in R.
+    result R( T.exec(query) );
+
+    // We're expecting to find some tables...
+    if (R.empty())
+    {
+        cout << "No tables found.  Cannot test.";
+        return tableSet;
+    }
+
+    // Process each successive result tuple, store the table names into the set
+    for (result::const_iterator c = R.begin(); c != R.end(); ++c)
+    {
+        // Dump tuple number and column 0 value to cout.  Read the value using
+        // as(), which converts the field to the same type as the default value
+        // you give it (or returns the default value if the field is null).
+//        cout << '\t' << to_string(c.num()) << '\t' << c[0].as(string()) << endl;
+        tableSet.insert(c[0].as(string()));
+    }
+
+    T.commit();
+
+    return tableSet;
+}
+
+
+set<string> listPostgresTables()
+{
+    std::set<std::string> tableSet;
+
+    const char query[] = "SELECT tablename FROM pg_tables where tablename "
+                         "not like 'pg_%' and tablename not like 'sql_%';";
+    connection C;
+
+    cout << "Connected to database: " << C.dbname() << endl
+        << "Backend version: " << C.server_version() << endl
+        << "Protocol version: " << C.protocol_version() << endl << endl;
+
+    // Begin a transaction acting on our current connection.  Give it a human-
+    // readable name so the library can include it in error messages.
+    nontransaction T(C, "test1");
+
+    // Perform the query on the database, storing result tuples in R.
+    result R( T.exec(query) );
+
+    // We're expecting to find some tables...
+    if (R.empty())
+    {
+        cout << "No tables found.  Cannot test.";
+        return tableSet;
+    }
+
+    // Process each successive result tuple, store the table names into the set
+    for (result::const_iterator c = R.begin(); c != R.end(); ++c)
+    {
+        // Dump tuple number and column 0 value to cout.  Read the value using
+        // as(), which converts the field to the same type as the default value
+        // you give it (or returns the default value if the field is null).
+//        cout << '\t' << to_string(c.num()) << '\t' << c[0].as(string()) << endl;
+        tableSet.insert(c[0].as(string()));
+    }
+
+    T.commit();
+
+    return tableSet;
+}
+
+
+void initialize()
+{
+    /* Initialization */
+    cout << " Initializing ... " << endl << endl;
+
+    /* Rasdaman tables */
+    set<string> rasTables = listRasdamanCoverages();
+    set<string>::iterator iter;
+    cout << rasTables.size() << " Rasdaman tables: " << endl;
+    for (iter = rasTables.begin(); iter != rasTables.end(); iter ++ )
+        cout << " - " << (*iter) << endl;
+    cout << " ------------ " << endl;
+
+    /* Postgres tables */
+    set<string> pgTables = listPostgresTables();
+    cout << pgTables.size() << " Postgres tables: " << endl;
+    for (iter = pgTables.begin(); iter != pgTables.end(); iter ++ )
+        cout << " - " << (*iter) << endl;
+    cout << " ------------ " << endl;
+
+    /* Create the table dictionary */
+    map<string, string> tableMap;
+    string rql = "Rasdaman";
+    string sql = "Postgres";
+    for (iter = rasTables.begin(); iter != rasTables.end(); iter ++ )
+        tableMap.insert(pair<string, string>(*iter, rql));
+    for (iter = pgTables.begin(); iter != pgTables.end(); iter ++)
+        if (tableMap.count(*iter) == 0)
+            tableMap.insert(pair<string, string>(*iter, sql));
+        else
         {
-            hqlQueries = " (nothing) ";
-            cout << "\n\n\n" << "HQL QUERIES\n" << hqlQueries << endl << endl << flush;
-            return 0;
+            cout << "\n ERROR ! Table " << *iter << " is a coverage name in Rasdaman AND "
+                    << " a table in Postgres. Cannot continue. " << endl;
+            exit(1);
         }
 
-//        runSqlQuery("SELECT * FROM pg_database");
-//        runSqlQuery(hqlQueries);
-        runRasqlQuery(hqlQueries);
+    /* Print the Table dictionary */
+    map<string, string>::iterator tuple;
+    cout << "Name dictionary : " << endl;
+    for (tuple = tableMap.begin(); tuple != tableMap.end(); tuple ++)
+        cout << " - " << (*tuple).first << " -> " << (*tuple).second << endl;
+    cout << endl;
 
+    cout << endl << " Initialization complete. " << endl;
+}
+
+int main()
+{
+    initialize();
         
-	return errorCount;
+    yyparse();
+
+    cout << endl << " Found " << errorCount << " syntax errors. " << endl << endl;
+
+    if (hqlQueries == NULL)
+    {
+        hqlQueries = (char *) " (nothing) ";
+        cout << "\n\n\n" << "HQL QUERIES\n" << hqlQueries << endl << endl << flush;
+        return errorCount;
+    }
+
+   runSqlQuery("SELECT * FROM pg_database");
+//   runSqlQuery(hqlQueries);
+//    runRasqlQuery(hqlQueries);
+
+
+    return errorCount;
 }
